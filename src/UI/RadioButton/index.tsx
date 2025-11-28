81dca239df7f0930014a85c60918eb10b30e9596
Alex Meshok <eisven@yandex.ru>
2025-11-28 11:18:29 +0300

    fixed raio button style

--- src/UI/RadioButton/index.tsx
+++ src/UI/RadioButton/index.tsx
@@ -3,6 +3,8 @@ import React, { memo, FC } from 'react';
 import RadioButtonOff from '~assets/radio-button-off.svg';
 import RadioButtonOn from '~assets/radio-button-on.svg';
 import { RADIO_BUTTON_ICON } from '~constants/dimensions';
+import { useAppSelector } from '~hooks';
+import { selectThemeScheme } from '~redux/selectors/theme';
 import { useThemeColors } from '~theme/hooks';
 
 export type Props = {
@@ -13,7 +15,9 @@ export type Props = {
 
 const RadioButton: FC<Props> = ({ isSelected, color, style }) => {
   const themeColors = useThemeColors();
-  const fillColor = color || themeColors.neutral_light;
+  const themeScheme = useAppSelector(selectThemeScheme);
+  const defaultColor = themeScheme === 'light' ? themeColors.primary_medium : themeColors.neutral_light;
+  const fillColor = color || defaultColor;
   return isSelected ? (
     <RadioButtonOn style={style} width={RADIO_BUTTON_ICON.width} height={RADIO_BUTTON_ICON.height} fill={fillColor} />
   ) : (
